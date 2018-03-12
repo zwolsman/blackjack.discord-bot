e3ae024f3a69c17b2c9d4fd24bb84a1a44317dfe
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-13 00:30:53 +0100

    Some extra logging

--- src/main/kotlin/com/zwolsman/blackjack/discordbot/command/listeners/CreateCommandListener.kt
+++ src/main/kotlin/com/zwolsman/blackjack/discordbot/command/listeners/CreateCommandListener.kt
@@ -11,16 +11,17 @@ class CreateCommandListener : UserAwareCommandHandler<CreateCommand>() {
     override val command = CreateCommand()
 
     override fun commandReceived(command: CreateCommand) {
-        logger.info("Yooooooo let's go!")
-        logger.info("${user.name} has ${user.guildPoints} guild points and wants to create a game mannn")
+        logger.info("${user.name} has ${user.guildPoints} guild points and wants to create a game in ${channel.guild.name}")
 
         if (Game.isOpenIn(command.channel.longID)) {
+            logger.error("There is already a game open #${channel.name}, ${channel.guild.name}")
             channel.sendMessage("There is already a game playing in this channel.")
             return
         }
         val minBuyIn = channel.getMinimalBuyIn()
 
         if (minBuyIn == null) {
+            logger.error("Can't find minimum bet amount for ${channel.name} in ${channel.guild.name}")
             sendError("Invalid channel to create a game, please use a channel that starts with `buy-in`")
             return
         }
@@ -29,9 +30,11 @@ class CreateCommandListener : UserAwareCommandHandler<CreateCommand>() {
 
         if (buyIn > user.guildPoints) {
             sendError("I'm sorry ${user.mention}, you have insufficient points to create a game and buy in with **${buyIn} server points**.")
+            logger.info("Buy in is too high for ${user.name} in ${channel.guild.name}")
             return
         } else if (buyIn < minBuyIn) {
             sendError("Buy in amount is **too low**, the minimum is **$minBuyIn server points**")
+            logger.info("Buy in is too low, minimum is $minBuyIn #${channel.name}, ${channel.guild.name}")
             return
         }
 
