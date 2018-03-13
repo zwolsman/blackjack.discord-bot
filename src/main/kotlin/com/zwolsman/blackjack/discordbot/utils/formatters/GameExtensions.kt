b71a99aad0238e0e9343ed603a323782de465078
Zwolsman,Marvin M.W <m.zwolsman@student.fontys.nl>
2018-03-13 13:37:58 +0100

    Append hands

--- src/main/kotlin/com/zwolsman/blackjack/discordbot/utils/formatters/GameExtensions.kt
+++ src/main/kotlin/com/zwolsman/blackjack/discordbot/utils/formatters/GameExtensions.kt
@@ -1,5 +1,6 @@
 package com.zwolsman.blackjack.discordbot.utils.formatters
 
+import com.zwolsman.blackjack.core.game.Hand
 import com.zwolsman.blackjack.discordbot.Config
 import org.jetbrains.exposed.sql.transactions.transaction
 import sx.blah.discord.handle.obj.IChannel
@@ -9,6 +10,7 @@ import java.util.*
 fun IChannel.sendMessage(game: com.zwolsman.blackjack.discordbot.entities.Game) {
     when (game.status) {
         0 -> openGame(game, this)
+        1 -> playingGame(game, this)
         else -> TODO("Handle game status ${game.status}")
     }
 }
@@ -36,6 +38,38 @@ private fun openGame(game: com.zwolsman.blackjack.discordbot.entities.Game, chan
 
 private fun EmbedBuilder.emptyField(inline: Boolean = true) = this.appendField("\u200E", "\u200E", inline)
 
-private fun playingGame(game: com.zwolsman.blackjack.discordbot.entities.Game) {
+private fun playingGame(game: com.zwolsman.blackjack.discordbot.entities.Game, channel: IChannel) {
+    val builder = EmbedBuilder()
+            .withTitle(":game_die: Game ${game.id}")
+            .withTimestamp(Date().toInstant())
 
+    if (game.history.isEmpty()) {
+        builder.appendField("History", "None", false)
+    } else {
+        builder.appendField("History", game.history, false)
+    }
+
+    transaction {
+        for ((user, player) in game.users.zip(game.instance.players)) {
+            val displayName = channel.guild.getUserByID(user.user.discordId).getDisplayName(channel.guild)
+            if (player.hands.size == 1) {
+                val hand = player.hands[0]
+                builder.appendHand(displayName, hand)
+                continue
+            }
+            for ((hid, hand) in player.hands.withIndex()) {
+                builder.appendHand("$displayName hand ${hid + 1}", hand)
+            }
+        }
+    }
+
+    channel.sendMessage(builder.build())
+}
+
+private fun EmbedBuilder.appendHand(name: String, hand: Hand) {
+
+    this.appendField(name, hand.cards.joinToString() { it.icon }, true)
+    this.appendField("Points", hand.points.joinToString(), true)
+
+    this.emptyField()
 }
